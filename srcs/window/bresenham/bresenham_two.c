543b888bfd9b59c543234db73ca4b39c088eda1f
gchopin <gchopin@42.student.fr>
2021-11-17 13:28:53 +0100

    push

--- srcs/window/bresenham/bresenham_two.c
+++ srcs/window/bresenham/bresenham_two.c
@@ -6,7 +6,7 @@
 /*   By: gchopin <gchopin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 11:25:30 by gchopin           #+#    #+#             */
-/*   Updated: 2021/11/17 11:00:33 by gchopin          ###   ########.fr       */
+/*   Updated: 2021/11/17 13:24:51 by gchopin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	fourth_segment(t_thread *thread,
 	dx = start->x - end->x;
 	dy = end->y - start->y;
 	*dp = 2 * dx - dy;
-	while (end->y >= start->y)
+	draw_pixel(thread, start->x, start->y);
+	while (end->y > start->y)
 	{
-		draw_pixel(thread, start->x, start->y);
 		if (*dp > 0)
 		{
 			*dp = *dp + 2 * (dx - dy);
@@ -34,6 +34,7 @@ void	fourth_segment(t_thread *thread,
 			*dp = *dp + (2 * dx);
 		}
 		start->y = start->y + 1;
+		draw_pixel(thread, start->x, start->y);
 	}
 }
 
@@ -46,9 +47,9 @@ void	third_segment(t_thread *thread,
 	dx = start->x - end->x;
 	dy = end->y - start->y;
 	*dp = 2 * dy - dx;
-	while (start->x >= end->x)
+	draw_pixel(thread, start->x, start->y);
+	while (start->x > end->x)
 	{
-		draw_pixel(thread, start->x, start->y);
 		if (*dp > 0)
 		{
 			*dp = *dp + (2 * (dy - dx));
@@ -59,6 +60,7 @@ void	third_segment(t_thread *thread,
 			*dp = *dp + (2 * dy);
 		}
 		start->x = start->x - 1;
+		draw_pixel(thread, start->x, start->y);
 	}
 }
 
@@ -71,9 +73,9 @@ void	second_segment(t_thread *thread,
 	dx = end->x - start->x;
 	dy = end->y - start->y;
 	*dp = 2 * dx - dy;
-	while (end->y >= start->y)
+	draw_pixel(thread, start->x, start->y);
+	while (end->y > start->y)
 	{
-		draw_pixel(thread, start->x, start->y);
 		if (*dp > 0)
 		{
 			*dp = *dp + (2 * (dx - dy));
@@ -84,6 +86,7 @@ void	second_segment(t_thread *thread,
 			*dp = *dp + (2 * dx);
 		}
 		start->y = start->y + 1;
+		draw_pixel(thread, start->x, start->y);
 	}
 }
 
@@ -96,9 +99,9 @@ void	first_segment(t_thread *thread,
 	dx = end->x - start->x;
 	dy = end->y - start->y;
 	*dp = 2 * dy - dx;
-	while (end->x >= start->x)
+	draw_pixel(thread, start->x, start->y);
+	while (end->x > start->x)
 	{
-		draw_pixel(thread, start->x, start->y);
 		if (*dp > 0)
 		{
 			*dp = *dp + (2 * (dy - dx));
@@ -109,5 +112,6 @@ void	first_segment(t_thread *thread,
 			*dp = *dp + (2 * dy);
 		}
 		start->x = start->x + 1;
+		draw_pixel(thread, start->x, start->y);
 	}
 }
